166c5ba5c55d264e76551dae023dd58d428b1e97
Andrey Savchenko <andrey@aejis.eu>
2013-05-20 20:56:35 +0300

    Kill mutant with typecasting in reader name

--- spec/unit/libclub/reader/name_spec.rb
+++ spec/unit/libclub/reader/name_spec.rb
@@ -1,10 +1,22 @@
 require 'spec_helper'
 
 describe Libclub::Reader, '#name' do
-  let(:instance)   { described_class.new('Barack') }
+  let(:instance)   { described_class.new(name) }
   let(:subject)    { instance.name }
 
-  it 'should assign name' do
-    expect(subject).to eql('Barack')
+  context 'when name is string' do
+    let(:name) { 'Barack' }
+
+    it 'should assign name' do
+      expect(subject).to eql('Barack')
+    end
+  end
+
+  context 'when name is symbol' do
+    let(:name) { :Barack }
+
+    it 'should assign name' do
+      expect(subject).to eql('Barack')
+    end
   end
 end
