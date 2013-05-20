4be006cb546b8e5d19146726a4875341f3025fa1
Andrey Savchenko <andrey@aejis.eu>
2013-05-20 20:29:36 +0300

    Kill mutant when push to books_borrowed is not actually covered

--- spec/unit/libclub/reader/borrow_spec.rb
+++ spec/unit/libclub/reader/borrow_spec.rb
@@ -5,10 +5,16 @@ describe Libclub::Reader, '#borrow' do
   let(:user)  { described_class.new('Barack')  }
   let(:book)  { mock('Book', owner: owner) }
 
-  it 'should make book unavailable for owner' do
+  before do
     owner.books_owned << book
     user.borrow(book)
+  end
 
+  it 'should make book unavailable for owner' do
     expect(owner.books_lent).to include(book)
+  end
+
+  it 'should add book to borrowed' do
+    expect(user.books_borrowed).to include(book)
   end
 end
